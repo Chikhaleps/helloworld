daa376cd7da25d81bb518b1617f00ae987765983
Prathamesh <prathameshchikhale130@gmail.com>
2022-04-04 18:41:18 +0530

    create Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
     public static void main(String[] args) {
         
         System.out.println("HelloWorld");
-        System.out.println("My Name is Prathamesh");
+        System.out.println(" Hello world ...!!");
     }
 
 }
